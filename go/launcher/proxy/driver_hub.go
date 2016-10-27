e6eebb641f5908fcd93c08d55f5d42afc5d771fb
Marc Fisher II <fisherii@google.com>
2016-10-27 10:28:55 -0700

    Revert debugging print.

--- go/launcher/proxy/driver_hub.go
+++ go/launcher/proxy/driver_hub.go
@@ -88,7 +88,7 @@ func (h *hub) routeToSession(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *hub) createSession(ctx context.Context, w http.ResponseWriter, r *http.Request) {
-	log.Printf("Creating session: %+v\n\n", r)
+	log.Print("Creating session\n\n")
 	var desired map[string]interface{}
 
 	if err := h.waitForHealthyEnv(ctx); err != nil {
