833498949f3525ccf6cb280f3419d8cac84943ed
Marc Fisher <fisherii@google.com>
2017-03-08 16:46:38 -0800

    Fix golint errors. (#104)

--- go/launcher/proxy/healthz/healthz.go
+++ go/launcher/proxy/healthz/healthz.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package healthx provides an HTTPHandler that always returns the string "ok".
+// Package healthz provides an HTTPHandler that always returns the string "ok".
 package healthz
 
 import (
