caac9154ea4157beea5a5f34c5c4912440eed748
Marc Fisher <fisherii@google.com>
2018-02-27 13:23:11 -0800

    Refactor wsl.go and add test. (#209)

---
+++ go/wsl/wsl_test.go
@@ -0,0 +1,186 @@
+package wsl
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/bazelbuild/rules_webtesting/go/portpicker"	
+)
+
+func TestHandleHealthz(t *testing.T) {
+	handler := createHandler(http.HandlerFunc(func(http.ResponseWriter, *http.Request) {}), func() {})
+
+	w := NewFakeResponseWriter()
+	r, err := http.NewRequest(http.MethodGet, "http://localhost/healthz", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler.ServeHTTP(w, r)
+
+	if w.status != http.StatusOK {
+		t.Errorf(`Got status %d, want %d`, w.status, http.StatusOK)
+	}
+
+	if !strings.Contains(string(w.Bytes()), "ok") {
+		t.Errorf(`Got %q, want to contain "ok"`, string(w.Bytes()))
+	}
+}
+
+func TestHandleQuitQuitQuit(t *testing.T) {
+	cancelCalled := 0
+
+	cancel := func() {
+		cancelCalled = cancelCalled + 1
+	}
+
+	handler := createHandler(http.HandlerFunc(func(http.ResponseWriter, *http.Request) {}), cancel)
+
+	w := NewFakeResponseWriter()
+	r, err := http.NewRequest(http.MethodGet, "http://localhost/quitquitquit", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler.ServeHTTP(w, r)
+
+	if w.status != http.StatusOK {
+		t.Errorf(`Got status %d, want %d`, w.status, http.StatusOK)
+	}
+
+	if !strings.Contains(string(w.Bytes()), "shutting down") {
+		t.Errorf(`Got %q, want to contain "shutting down"`, string(w.Bytes()))
+	}
+
+	if cancelCalled != 1 {
+		t.Errorf("Cancel was called %d times, want 1 time", cancelCalled)
+	}
+}
+
+func TestHandleSession(t *testing.T) {
+	hubCalled := 0
+
+	hub := http.HandlerFunc(func(http.ResponseWriter, *http.Request) {
+		hubCalled = hubCalled + 1
+	})
+
+	handler := createHandler(hub, func() {})
+
+	r, err := http.NewRequest(http.MethodGet, "http://localhost/session", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler.ServeHTTP(NewFakeResponseWriter(), r)
+
+	if hubCalled != 1 {
+		t.Errorf("Hub was called %d times, want 1 time", hubCalled)
+	}
+
+	r, err = http.NewRequest(http.MethodGet, "http://localhost/session/id", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler.ServeHTTP(NewFakeResponseWriter(), r)
+
+	if hubCalled != 2 {
+		t.Errorf("Hub was called %d times, want 2 time", hubCalled)
+	}
+}
+
+func TestHandleRoot(t *testing.T) {
+	handler := createHandler(http.HandlerFunc(func(http.ResponseWriter, *http.Request) {}), func() {})
+
+	w := NewFakeResponseWriter()
+	r, err := http.NewRequest(http.MethodGet, "http://localhost/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler.ServeHTTP(w, r)
+
+	if w.status != http.StatusNotFound {
+		t.Errorf(`Got status %d, want %d`, w.status, http.StatusNotFound)
+	}
+}
+
+func TestStartAndShutdownServer(t *testing.T) {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		io.WriteString(w, "ok")
+	})
+
+	port, err := portpicker.PickUnusedPort()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	errChan := make(chan error)
+
+	go func () {
+		errChan <- startServer(ctx, port, handler)
+	}()
+
+	// Give server plenty of time to get started
+	time.Sleep(100*time.Millisecond)
+
+	addr := fmt.Sprintf("http://localhost:%d", port)
+
+	resp, err := http.Get(addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("Got %d status, expected 200", resp.StatusCode)
+	}
+
+	cancel()
+
+	timeoutCtx, timeoutCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer timeoutCancel()
+
+	select {
+	case <-timeoutCtx.Done():
+		t.Fatal("expected server to stop with 5 seconds")
+	case err := <-errChan:
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	_, err = http.Get(addr)
+	if err == nil {
+		t.Fatal("Got nil err, want err")
+	}
+}
+
+func NewFakeResponseWriter() *fakeResponseWriter {
+	return &fakeResponseWriter{
+		Buffer: &bytes.Buffer{},
+		status: http.StatusOK,
+		header: http.Header(map[string][]string{}),
+	}
+}
+
+type fakeResponseWriter struct {
+	*bytes.Buffer
+	status int
+	header http.Header
+}
+
+func (frw *fakeResponseWriter) Header() http.Header {
+	return frw.header
+}
+
+func (frw *fakeResponseWriter) WriteHeader(status int) {
+	frw.status = status
+}
