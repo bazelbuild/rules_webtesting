6e5905b85cfad8006bae4b16d986e79deff06b13
Marc Fisher <fisherii@google.com>
2018-05-18 12:23:16 -0700

    Cleanup more lint errors.

--- go/metadata/metadata.go
+++ go/metadata/metadata.go
@@ -194,7 +194,7 @@ func (m *Metadata) GetFilePath(name string) (string, error) {
 	return "", fmt.Errorf("no named file %q", name)
 }
 
-// Resolver returns a Resolver that process ENV, FILE, and METADATA prefixed
+// Resolver returns a Resolver that processes ENV, FILE, and METADATA prefixed
 // capabilities variables.
 func (m *Metadata) Resolver() capabilities.Resolver {
 	metadataResolver := capabilities.MapResolver("METADATA", map[string]string{
