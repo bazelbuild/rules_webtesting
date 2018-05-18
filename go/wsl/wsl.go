5704343b16fa505c1855a8ec2f0fc42188e400cb
Marc Fisher <fisherii@google.com>
2018-05-18 11:21:19 -0700

    Cleanup.

--- go/wsl/wsl.go
+++ go/wsl/wsl.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package wsl impleements the basic server code for WSL (Webdriver Server Light),
+// Package wsl implements the basic server code for WSL (Webdriver Server Light),
 // a lightweight replacement for Selenium Server.
 package wsl
 
