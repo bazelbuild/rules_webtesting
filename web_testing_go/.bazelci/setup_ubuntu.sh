581004aca302383e1adf04df01d165c5fb626e77
Yuan Tian <93746690+mollyibot@users.noreply.github.com>
2025-05-29 10:44:09 -0700

    Copy setup_ubuntu.sh used in submodules (#500)
    * Update the setup.sh as symlink
    
    * More polish
    
    * Update symlinks to be real file

---
+++ web_testing_go/.bazelci/setup_ubuntu.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# Copyright 2025 Google Inc. All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Script that can be used by CI server for testing rules_webtesting builds.
+set -ex
+
+sudo apt -y update
+sudo apt -y install libasound2 \
+                    libdbus-glib-1-2 \
+                    libatk-bridge2.0-0 \
+                    libatk1.0-0 \
+                    libc6 \
+                    libcairo2 \
+                    libcups2 \
+                    libdbus-1-3 \
+                    libexpat1 \
+                    libfontconfig1 \
+                    libgbm1 \
+                    libgcc1 \
+                    libglib2.0-0 \
+                    libgtk-3-0 \
+                    libnspr4 \
+                    libnss3 \
+                    libpango-1.0-0 \
+                    libpangocairo-1.0-0 \
+                    libstdc++6 \
+                    libx11-6 \
+                    libx11-xcb1 \
+                    libxcb1 \
+                    libxcomposite1 \
+                    libxcursor1 \
+                    libxdamage1 \
+                    libxext6 \
+                    libxfixes3 \
+                    libxi6 \
+                    libxrandr2 \
+                    libxrender1 \
+                    libxss1 \
+                    libxtst6 \
+                    libgtk-3-0
